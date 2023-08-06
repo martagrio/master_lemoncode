8f2d43b3d661fb16c8ecec1a7f2d4a2c42616b1b
martagrio <marta.garcia@incamedio.com>
2023-08-06 22:01:36 +0200

    Finish ejercicio_reto

--- 01_modulo_layout/layout_reto/dist/layout_reto.59712999.js
+++ 01_modulo_layout/layout_reto/dist/layout_reto.59712999.js
@@ -142,7 +142,7 @@
       this[globalName] = mainExports;
     }
   }
-})({"l2tSJ":[function(require,module,exports) {
+})({"7vniE":[function(require,module,exports) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
@@ -575,6 +575,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"4M6V8":[function(require,module,exports) {
 
-},{}]},["l2tSJ","4M6V8"], "4M6V8", "parcelRequire1230")
+},{}]},["7vniE","4M6V8"], "4M6V8", "parcelRequire1230")
 
 //# sourceMappingURL=layout_reto.59712999.js.map
